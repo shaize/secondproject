ac339e413c610cb3e6270df9349d4ead42ba760b
shaize <nym14@hanmail.net>
2017-06-28 17:41:19 +0900

    added output "BitKeeper"

--- Hello.java
+++ Hello.java
@@ -2,7 +2,8 @@ public class Hello{
 	public static void main(String [] args){
 		System.out.println("Hello Git");
 		System.out.println("What is Git?");
-			
+		System.out.println("2002 BitKeeperDVCS");
+
 	}
 }
 
