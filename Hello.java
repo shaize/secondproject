2d4eba95a7e653bef7edebe1df1a7fcd3e9b8dc5
shaize <nym14@hanmail.net>
2017-07-24 17:18:05 +0900

    Hello.java modified on GItHub

--- Hello.java
+++ Hello.java
@@ -1,10 +1,13 @@
+//For Command line git tutorial
+//GitHub modification
+
 public class Hello{	
 	public static void main(String [] args){
 		System.out.println("Hello Git");
 		System.out.println("What is Git?");
 		System.out.println("That's DVCS");	
 		System.out.println("2002 BitKeeperDVCS");
-
+		
 
 	}
 }
